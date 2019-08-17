295b6eef403d7f0c9e34dfba8feb1c110220e619
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-17 13:07:27 +0300

    notes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//inx_001
 const http = require( 'http' );
 const fs   = require( 'fs' );
 
+        //inx_002
 const server = http.createServer( ( req, res ) => {
+                                    //inx_004
     fs.writeFile( './data/users', 'Pesho Atanasov Peshev', ( err ) => {
         if ( err ) {
             debugger;
@@ -14,4 +17,5 @@ const server = http.createServer( ( req, res ) => {
     res.end();
 } );
 
+        //inx_003
 server.listen( 3000 );
