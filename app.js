75d3b9d5d089ef11993b20b8c15022723301d7c4
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-18 06:30:09 +0300

    some cleanup

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const http    = require( 'http' );
-const fs      = require( 'fs' );
 const express = require( 'express' );
-
-const app    = express();
+const app     = express();
 
 const routesLibrary = require( './routes/library' );
 
